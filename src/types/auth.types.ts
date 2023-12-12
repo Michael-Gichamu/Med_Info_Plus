f7c1dc79bc8ef78393d130d86cc51d29a9785931
Michael-Gichamu <michaelmatere606@gmail.com>
2023-12-12 13:10:00 +0300

    feat:authenticate and add homepage

---
+++ src/types/auth.types.ts
@@ -0,0 +1,12 @@
+export interface ILoginData {
+  email: string;
+  password: string;
+}
+
+export interface IsignupData {
+  name: string;
+  email: string;
+  password: string;
+  confirmpassword: string;
+  provider: boolean;
+}
